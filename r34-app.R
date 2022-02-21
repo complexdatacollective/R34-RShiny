0a8496f88d35590099942ca1f2f40b5b9c2b31d8
krisher1 <krisher@ic.ac.uk>
2022-02-21 14:59:03 -0600

    Update RShiny app
    New version that takes .zipped file of CSVs and cleans the data to output a data table that matches the CHIMS input screen

--- r34-app.R
+++ r34-app.R
@@ -1,94 +1,132 @@
 #App to display Partner Services data collected in Network Canvas
 
-#install dependencies
-install.packages("shiny")
+#install dependencies if needed
+if (!require(shiny))  install.packages("shiny")
+# if (!require(igraph)) install.packages("igraph")
+if (!require(dplyr)) install.packages("dplyr")
+if(!require(DT)) install.packages("DT")
 
 #load libraries
-library(shiny)
+library(shiny); library(dplyr); library(DT)
+# library(igraph)
+
+# source some data cleaning functions
+source("r34_cleaning.R")
 
 #UI
-ui <- fluidPage(
-  
-  # App title
-  titlePanel("Partner Services Network Canvas Data Upload"),
-  
-  # Sidebar 
-  sidebarLayout(
+ui <- navbarPage("Partner Services Network Canvas Data Upload",
+    tabPanel("Data",
+             # Sidebar 
+             sidebarLayout(
+                 # Sidebar panel 
+                 sidebarPanel(
+                     
+                     # Input: Select a file 
+                     fileInput("ego_data", "Upload .zip File",
+                               multiple = TRUE,
+                               accept = c(".zip")),
+                 ),
+                 mainPanel(
+                     "Something to show the data were correctly loaded here"
+                           )
     
-    # Sidebar panel 
-    sidebarPanel(
-      
-      # Input: Select a file 
-      fileInput("ego_data", "Upload .graphml File",
-                multiple = TRUE,
-                accept = c(".graphml")),
-    ), 
-    
-    # Main panel for displaying outputs 
-    mainPanel(
-      
-      titlePanel("Question Package Name"),
-      
-      # Output
-      column(3, 
-             verbatimTextOutput("caseID")),
-      column(10, 
-             tableOutput("caseid")),
-      column(3, 
-             verbatimTextOutput("sexTypes")),
-      column(10, 
-             tableOutput("sextypes")),
-      column(3, 
-             verbatimTextOutput("condomUse")),
-      column(10, 
-             tableOutput("condomuse")),
-      column(10, 
-             tableOutput("alldata")),
-      
-    )
-    
-  )
+             )
+                 
+    ),
+    navbarMenu("Sexual behavior",
+               tabPanel("Within Interview Period",
+                        "Blank for now"
+                     ),
+               tabPanel("Within 12 months",
+                        fluidRow(
+                            column(12,
+                                   DTOutput("sexbehav12m"))
+                        )
+                        
+                        )
+               ),
+    navbarMenu("Substance Use",
+               tabPanel("Within Interview Period",
+                        "Blank for now"),
+               tabPanel("Within 12 months",
+                        "Blank for now"),
+               ),
+    tabPanel("Referral contacts",
+             "Blank for now")
+
+
 )
 
 #server
 server <- function(input, output) {
   
+    graph_dat <- reactive({
+        
+        req(input$ego_data)
+        
+        return(data_cleaning(input$ego_data$datapath))
+        
+        # igraph::read_graph(input$ego_data$datapath,"graphml")
+        
+    }) # now can use graph_dat() to call the data throughout
+    
   output$alldata <- renderTable({
-    
-    req(input$ego_data)
-    
-    df <- read.csv(input$ego_data$datapath)
-    
-    return(df)
+      
+      req(input$ego_data)
+      
+      ego_dat <- graph_dat()$egodat
+      
+      # ego_dat <- unlist(igraph::graph_attr(graph_dat()))
+      
+      return(ego_dat)
   }) 
   
   output$caseid <- renderTable({
-    
-    req(input$ego_data)
-    
-    df <- read.csv(input$ego_data$datapath)
-    
-    return(df[1,2])
+      
+      req(input$ego_data)
+      
+      # vertices_dat <- igraph::as_data_frame(graph_dat(),"vertices")
+      
+      # vertices_dat$id
+      
   }) 
   
   output$sextypes <- renderTable({
     
-    req(input$ego_data)
+      req(input$ego_data)
+      
+      # ego_dat <- igraph::graph_attr(graph_dat())
     
-    df <- read.csv(input$ego_data$datapath)
+    # df <- read.csv(input$ego_data$datapath)
     
-    data <- c(df[1, 17], df[1,18], df[1,19])
+    # data <- c(df[1, 17], df[1,18], df[1,19])
     return(data)
   }) 
   
   output$condomuse <- renderTable({
     
-    req(input$ego_data)
-    
-    df <- read.csv(input$ego_data$datapath)
-    
-    data <- c(df[1, 12], df[1,13], df[1,14], df[1,15], df[1,16])
-    return(data)
+      req(input$ego_data)      
+      
+      
+  })
+  
+  # output$sexbehav12m <- renderTable({
+  #     req(input$ego_data)
+  # 
+  #     return(graph_dat()$sexbehav12m)
+  # }, bordered = TRUE)
+  
+  output$sexbehav12m <- renderDT({
+      req(input$ego_data)
+      
+      data <-   DT::datatable(graph_dat()$sexbehav12m,
+                              options = list(pageLength = 25),
+                              class = "cell-border stripe",
+                              rownames = FALSE,
+                              caption = "Sexual Behaviors within 12 Months",
+                              escape = FALSE)
+      
+      return(data)
   })
   
   #labels
@@ -98,3 +136,6 @@ server <- function(input, output) {
 }
 # Run the app 
 shinyApp(ui, server)
+
+#### NOTES:
+# is this going to be hosted on a web server? if yes, need to save this as "app.R"? (or could have ui.R and server.R)
---
+++ r34_cleaning.R
@@ -0,0 +1,159 @@
+
+
+data_cleaning <- function(indat) {
+    
+    filenames <- unzip(indat,list=TRUE)$Name
+    # Read in and clean the ego data
+    egofile <- filenames[grep("ego.csv",filenames)]
+    egodat <- read.csv(unz(indat,egofile))
+    egodat <- egodat %>%
+        dplyr::mutate(condoms12m = case_when(condoms_anal_insertive %in% c("sometimes","no") |
+                                                 condoms_anal_receptive %in% c("sometimes","no") |
+                                                 condoms_vaginal %in% c("sometimes","no") ~ "Y - Yes, Anal or Vaginal intercourse (with or without oral sex)",
+                                             condoms_oral_receptive %in% c("sometimes","no") | 
+                                                 condoms_oral %in% c("sometimes","no") ~ "O - Oral sex only",
+                                             condoms_anal_insertive=="yes" & condoms_anal_receptive=="yes" &
+                                                 condoms_vaginal=="yes" & condoms_oral_receptive=="yes" &
+                                                 condoms_oral=="yes" ~ "N - No",
+                                             TRUE ~ "UNK - Unknown"),
+                      condoms12m_pos = case_when(condoms_anal_insertive %in% c("sometimes","no") &
+                                                     condoms_anal_receptive %in% c("sometimes","no") ~ "B - Both",
+                                                 condoms_anal_insertive %in% c("sometimes","no") ~ "I - Insertive",
+                                                 condoms_anal_receptive %in% c("sometimes","no") ~ "R - Receptive",
+                                                 TRUE ~ NA_character_  ),
+                      sexual_identity = recode(sexual_identity, `1` = "Heterosexual", `2` = "Gay or Lesbian",
+                                               `3` = "Bisexual", `4` = "Unknown", `5` = "Other", .default=NA_character_),
+                      role = recode(role_self, "top" = "I - Insertive", "bottom" = "R - Receptive",
+                                    "vers" = "B - Both"),
+                      sex_high = recode(sex_under_influence, "y_anal_vaginal" = "Yes, Anal or Vaginal intercourse (with or without oral sex)",
+                                        "y_oral" = "Oral sex only", "n" = "No"))
+    
+    # Read in and clean the person attribute data
+    person_attr_file <- filenames[grep("attributeList_Person.csv",filenames)]
+    person_attr <- read.csv(unz(indat,person_attr_file))
+    person_attr <- person_attr %>%
+        dplyr::mutate_at(vars("sex_partner","race_white","race_black","race_asian","race_hisp",
+                           "gender_cis_male","gender_cis_female","gender_trans_male",
+                           "gender_trans_female","pregnant_yes","pregnant_no","partner_type_regular",
+                           "partner_type_fwb","partner_type_anon","venue_met_internet","venue_met_bar",
+                           "venue_met_bath","condoms_yes","condoms_sometimes","condoms_no","benefit_from_test",
+                           "bar_specific_1","bar_specific_2","bar_specific_3","bar_specific_4",
+                           "bar_specific_5","bar_specific_6","bar_specific_7","internet_specific_1",
+                           "internet_specific_2","internet_specific_3","internet_specific_4",
+                           "internet_specific_5","internet_specific_6","internet_specific_7","drug_use_partner"),
+                         ~recode(., `false`= FALSE, `true` = TRUE, .default = NA))
+    allpartners_dat <- person_attr %>%
+        summarise(n_cisf = sum(gender_cis_female==TRUE & sex_partner==TRUE,na.rm=TRUE),
+                  n_cism = sum(gender_cis_male==TRUE & sex_partner==TRUE, na.rm=TRUE),
+                  n_trans = sum((gender_trans_female==TRUE | gender_trans_male==TRUE) &
+                                    sex_partner==TRUE, na.rm=TRUE),
+                  n_anon = sum(partner_type_anon==TRUE & sex_partner==TRUE,na.rm=TRUE))
+
+    
+
+    # Read in and clean the venue attribute data
+    venue_attr_file <- filenames[grep("attributeList_Venue.csv",filenames)]
+    venue_attr <- read.csv(unz(indat,venue_attr_file))
+    
+    venue_summary <- venue_attr %>%
+        summarise(any_online = ifelse(sum(type=="app")>0,"Y - Yes","N - No"))
+    
+    # Read in and clean the edge list of sex partners
+    sex_edgelist_file <- filenames[grep("edgeList_had_sex.csv",filenames)]
+    sex_edgelist <- read.csv(unz(indat,sex_edgelist_file))
+    
+    # Read in and clean edge list of partners who know one another
+    know_edgelist_file <- filenames[grep("edgeList_know.csv",filenames)]
+    know_edgelist <- read.csv(unz(indat,know_edgelist_file))
+    
+    # Read in and clean edge list of shared needles
+    needles_edgelist_file <- filenames[grep("edgeList_shared_needles.csv",filenames)]
+    needles_edgelist <- read.csv(unz(indat,needles_edgelist_file))
+    
+    # no longer using graphml file
+    # dat <- igraph::read_graph(indat,"graphml")
+    # egodat <- igraph::graph_attr(dat)
+    # egodat <- do.call(cbind.data.frame,egodat)
+    # alterdat <- igraph::as_data_frame(dat,"vertices")
+    # edgesdat <- igraph::as_data_frame(dat,"edges")
+    
+    # library(xml2)
+    # dat <- xml2::read_xml(indat)
+    # 
+    # library(XML)
+    # test <- xmlParse(indat)
+    # test2 <- xmlToList(test)
+    
+    refperiod <- c("in past 12 months?", "during the interview period?")
+    sexbehavqs <- list()
+    for(i in 1:length(refperiod)) {
+        sexbehavqs[[i]] <- c(paste0("<b>Had sex with a female ",refperiod[i],"</b>"),
+                            "Type of Sex with female partner(s)",
+                            "Number of female partners",
+                            paste0("<b>Had sex with a male ",refperiod[i],"</b>"),
+                            "Type of Sex with male partner(s)",
+                            "Positioning (anal only)",
+                            "Number of male partners",
+                            "Heterosexual contact with bisexual male",
+                            paste0("<b>Had sex with a transgender person ",refperiod[i],"</b>"),
+                            "Type of Sex with transgender partner(s)",
+                            "Number of transgender partners",
+                            paste0("<b>Had sex with an anonymous partner ",refperiod[i],"</b>"),
+                            "Type of Sex with anonymous partner(s)",
+                            "Number of anonymous partners",
+                            paste0("<b>Had sex without using a condom ",refperiod[i],"</b>"),
+                            "Type of Sex without using a condom",
+                            paste0("<b>Had sex while intoxicated or high on drugs ",refperiod[i],"</b>"),
+                            "Type of Sex while intoxicated or high",
+                            paste0("<b>Exchanged drugs/money/goods for sex ",refperiod[i],"</b>"),
+                            "Type of Sex exchanged",
+                            "Transaction Type",
+                            paste0("<b>Had sex with a person who is known to be an MSM ",refperiod[i],"</b>"),
+                            "Type of Sex with known MSM",
+                            paste0("<b>Had sex with a person who is known to be an IDU ",refperiod[i],"</b>"),
+                            "Type of Sex with known IDU",
+                            paste0("<b>Had sex with a bisexual person ",refperiod[i],"</b>"),
+                            "Gender of bisexaul person",
+                            paste0("<b>Had sex with a person with AIDS or documented HIV infection ",refperiod[i],"</b>"),
+                            "Gender of person with HIV",
+                            paste0("<b>Met partners through the Internet ",refperiod[i],"</b>"))
+    }
+    
+    sexbehav12mind <- c(# A bunch of these outputs can't be filled yet because data collection not consistent
+                        # with CHIMS... 
+                        "","",allpartners_dat$n_cisf,
+                        "","",egodat$role,allpartners_dat$n_cism,
+                        "","","",allpartners_dat$n_trans,
+                        "","",allpartners_dat$n_anon,
+                        egodat$condoms12m, egodat$condoms12m_pos,
+                        egodat$sex_high,"",
+                        "","","","","","","","","","","",
+                        venue_summary$any_online)
+    
+    sexbehav12m <- data.frame(Questions = sexbehavqs[[1]],
+                              Responses = sexbehav12mind)
+    
+    druguseqs <- list()
+    for(i in 1:length(refperiod)) {
+        druguseqs[[i]] <- c(paste0("<b>Any alcohol or drug use ",refperiod[i]," (injection or non-injection)</b>"),
+                            "<b>Alcohol</b>","Frequency of use","Units",
+                            "<b>Crack</b>","Frequency of use","Units",
+                            "<b>Cocaine</b>","Frequency of use","Units",
+                            "<b>Heroin</b>","Frequency of use","Units",
+                            "<b>Methamphetamine</b>","Frequency of use","Units",
+                            "<b>Nitrates/poppers</b>","Frequency of use","Units",
+                            "<b>Erectile dysfunction medications</b>","Frequency of use","Units",
+                            "<b>Marijuana</b>","Frequency of use","Units",
+                            "<b>Other drugs</b>","Specify drug(s)","Frequency of use","Units",
+                            paste0("<b>Engaged in injection drug use ",refperiod[i],"</b>"),
+                            paste0("Shared injection drug equipment ",refperiod[i]))
+    }
+
+    
+    alldat <- list(egodat = egodat, person_attr = person_attr,
+                   venue_attr = venue_attr, sex_edgelist = sex_edgelist,
+                   know_edgelist = know_edgelist, needles_edgelist = needles_edgelist,
+                   sexbehav12m = sexbehav12m)
+    
+    return(alldat)
+}
